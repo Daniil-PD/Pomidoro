add978a69a75b8889b22f0ab639e42d6e20220c9
jennybez <72353288+jennybez@users.noreply.github.com>
2020-10-12 12:31:04 +0400

    Скелет окна вывода (часть 1). Ошибки исправлю после написания всей части кода

--- src/com/company/Interactive_menu.java
+++ src/com/company/Interactive_menu.java
@@ -2,8 +2,33 @@
 
 package com.company;
 
+import javax.swing.*;
+import java.awt.*;
 
-public class Interactive_menu extends Thread
+class Int_Menu_Thread extends Thread
+{
+
+}
+public class Interactive_menu extends JFrame // оконный вывод информации - расположение кнопок, вывод таймера
+{
+    private int time; //подумать как вывести с класса Clock_Mechanism тип вывода времени!!! (после того, как Вова собственно напишет)
+    private JLabel countTime;// Содержание вывода времени
+    private JButton Stop; //переменные для кнопок
+    private JButton Resume;
+
+    public Interactive_menu() {
+        super("Pomodoro");
+        countTime = new JLabel("Time" + time);// подумать как можно сделать рамку часов, изменить ее цвет+увеличить цифры
+        Stop = new JButton("Stop");
+        Resume = new JButton("Resume");
+        JPanel buttonsPanel = new JPanel(new FlowLayout());// объект панельки кнопок
+        add(countTime, BorderLayout.CENTER); // добавление расположения таймера
+        buttonsPanel.add(Stop);
+        buttonsPanel.add(Resume);
+        add(buttonsPanel, BorderLayout.SOUTH); // добавление панели кнопок вниз посередине
+        // ТУТ ДОЛЖНЫ ИСПОЛЬЗОВАТЬСЯ МЕТОДЫ таймера
+    }
+    // описание метода остановки и продолжения таймера с внутренним анонимным классом
 {
     @Override
     public void run(){
@@ -11,3 +36,4 @@ public class Interactive_menu extends Thread
     }
     
 }
+//не забыть добавить в Main строки по окну при окончании написания скелета!
