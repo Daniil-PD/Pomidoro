d980737c39a15bfc00a4daeeaf2c8f199e2f285b
StarostinRV <r.starostin2000@yandex.ru>
2020-11-15 03:26:02 +0400

    Roma:add comments to Mechanism_timer, Mechanism_stopwatch

--- src/com/company/Mechanism_stopwatch.java
+++ src/com/company/Mechanism_stopwatch.java
@@ -1,3 +1,23 @@
+/*
+     *Секундомер
+     *
+     *Конструктор:
+     * -Mechanism_stopwatch()        : Инициализирует начальные значения.
+     *
+     * Своства:
+     *
+     * boolean flag_pause            : Состояние секундомера
+     * static long time_start        : Время снятия секундомера с паузы
+     * static long time_that_passed  : Пройденое время с работы секундомера до паузы
+     *
+     * Методы:
+     * -long get_past_time           : Возвращает количество милисекунд прошедших после запуска секундомера
+     * -void start_mechanism()       : Запуск секундамера
+     * -void pause_mechanism()       : Ставит секундамер на паузу
+     * -void stop_mechanism()        : Останавливает/Сбрасывает секундамер
+     * -String state_mechanism()     : Возвращает состояние секундомера
+ */
+
 package com.company;
 
 public class Mechanism_stopwatch
@@ -5,42 +25,44 @@ public class Mechanism_stopwatch
     boolean flag_pause;
     static long time_start;
     static long time_that_passed;
-     
-    public Mechanism_stopwatch()
+
+    public Mechanism_stopwatch() // Ининциализация
     {
         flag_pause = true;
         time_that_passed = 0;
     }
 
-    public long get_past_time()
+    public long get_past_time() // Время работы секундамера
     {
-        if (flag_pause) return time_that_passed;
-        else return  System.currentTimeMillis() - time_start;
+        if (flag_pause) return time_that_passed; // при состояннии паузы
+        else return  System.currentTimeMillis() - time_start; // при состояннии работы
     }
 
-    public void start_mechanism()
+    public void start_mechanism() //Запуск
     {
-        if (!flag_pause) return;
-        time_start = System.currentTimeMillis();
-        time_start -= time_that_passed;
-        flag_pause = false;
+        if (!flag_pause) return; // проверка состояния
+        time_start = System.currentTimeMillis(); // время отсчета
+        time_start -= time_that_passed; // сдвиг из-за паузы
+        flag_pause = false; //смена состояния
     }
 
-    public void pause_mechanism()
+    public void pause_mechanism() //Пауза
     {
-        if(flag_pause) return;
-        time_that_passed = System.currentTimeMillis() - time_start;
-        flag_pause = true;
+        if(flag_pause) return; // Проверка состояния
+        time_that_passed = System.currentTimeMillis() - time_start; // Расчет пройденного времени до паузы
+        flag_pause = true; //Смена состояния
     }
     
     public void stop_mechanism()
     {
-        if (!flag_pause) return;
+        if (!flag_pause) return; // Проверка состояния
+        // Ининциализация
         flag_pause = true;
         time_that_passed = 0;
     }
 
-    public String state_mechanism(){
+    public String state_mechanism() // Возврат состояния секундомера
+    {
         if (flag_pause)
         {
             return "pause";
--- src/com/company/Mechanism_timer.java
+++ src/com/company/Mechanism_timer.java
@@ -1,14 +1,37 @@
+/*
+     *Таймер
+     *
+     *Конструктор:
+     * -Mechanism_stopwatch()              : Инициализирует начальные значения.
+     *
+     * Своства:
+     *
+     * boolean flag_pause                  : Состояние таймера
+     * long time_start                     : Время снятия таймера с паузы
+     * long time_that_passed               : Пройденое время с работы таймера до паузы
+     * long time_timer                     : Продолжительность работы таймера
+     *
+     * Методы:
+     * -void run()                         : Цикл жизни(работы) таймера
+     * setTime_timer(long time)            : Задаем значение переменной time_timer
+     * -long get_remaining_time            : Возвращает продолжительность после запускатаймера работы таймера в милисекунд
+     * -void start_mechanism()             : Запуск таймера
+     * -void pause_mechanism()             : Ставит таймер на паузу
+     * -void stop_mechanism()              : Останавливает/Сбрасывает таймер
+     * -void plusTime_mechanism(long time) : Добление доплнительного времени работы
+     * -String state_mechanism()           : Возвращает состояние таймера
+ */
+
 package com.company;
 
 public class Mechanism_timer extends Thread
 {
     long time_start;
     boolean flag_pause;
-    long work_timer;
     long time_timer;
     long time_that_passed;
 
-    public Mechanism_timer()
+    public Mechanism_timer() //Ининциализация
     {
         flag_pause = true;
         time_that_passed = 0;
@@ -17,13 +40,10 @@ public class Mechanism_timer extends Thread
     @Override
     public void run()
     {
-        //предлагаю его включить до инициализации
-        // таймера и профиля или перенести сист время
-
         time_start = System.currentTimeMillis();
-        while (true)
+        while (true) // цикл жизни
         {
-            if (get_remaining_time() <= 0)
+            if (get_remaining_time() <= 0) //Условине остановки таймера
             {
                 stop_mechanism();
                 Main.timer_finish();
@@ -35,53 +55,52 @@ public class Mechanism_timer extends Thread
                 e.printStackTrace();
             }
         }
-
     }
 
-    public void setTime_timer(long time_timer)
+    public void setTime_timer(long time_timer) //Установка времени
     {
         this.time_timer = time_timer;
     }
 
-    public long get_remaining_time()
+    public long get_remaining_time() //оставшееся время работы таймера
     {
-        if (flag_pause) return time_timer - time_that_passed;
-        else return time_timer - (System.currentTimeMillis() - time_start);
+        if (flag_pause) return time_timer - time_that_passed; //при состояннии паузы
+        else return time_timer - (System.currentTimeMillis() - time_start); // при состояннии работы
     }
 
     public void start_mechanism()
     {
-        if (!flag_pause) return;
-        time_start = System.currentTimeMillis();
-        time_start -= time_that_passed;
-        flag_pause = false;
+        if (!flag_pause) return; // Проверка состояния
+        time_start = System.currentTimeMillis(); // время отсчета
+        time_start -= time_that_passed; // сдвиг из-за паузы
+        flag_pause = false; //смена состояния
     }
 
     public void pause_mechanism()
     {
-        if(flag_pause) return;
-        time_that_passed = System.currentTimeMillis() - time_start;
-        flag_pause = true;
+        if(flag_pause) return; // Проверка состояния
+        time_that_passed = System.currentTimeMillis() - time_start;// Расчет пройденного времени до паузы
+        flag_pause = true; //Смена состояния
     }
 
     public void stop_mechanism()
     {
-        if (!flag_pause) return;
+        if (!flag_pause) return; // Проверка состояния
+        // Ининциализация
         flag_pause = true;
         time_that_passed = 0;
     }
 
-    public void plusTime_mechanism(long time)
+    public void plusTime_mechanism(long time) //Добление времени
     {
         if (flag_pause) return;
-        time_start -= time;
+        time_start -= time; //сдвиг из-за доп времени
     }
-    public String state_mechanism(){
+    public String state_mechanism() // Возврат состояния таймера
+    {
         if (flag_pause){
             return "pause";
         }
-
-
         return null;
     }
 
