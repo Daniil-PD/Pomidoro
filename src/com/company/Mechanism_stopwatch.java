ae30e4f5e8ea0fdf7924694a98320890e1bb7bbd
StarostinRV <r.starostin2000@yandex.ru>
2020-11-14 19:39:31 +0400

    Roma Mechanism_stopwatch

--- src/com/company/Mechanism_stopwatch.java
+++ src/com/company/Mechanism_stopwatch.java
@@ -1,4 +1,49 @@
 package com.company;
 
-public class Mechanism_stopwatch {
+public class Mechanism_stopwatch
+{
+    boolean flag_pause;
+    static long time_start;
+    static long time_that_passed;
+     
+    public Mechanism_stopwatch()
+    {
+        flag_pause = true;
+        time_that_passed = 0;
+    }
+
+    public long get_past_time()
+    {
+        if (flag_pause) return time_that_passed;
+        return  System.currentTimeMillis() - time_start;
+    }
+
+    public void start_mechanism()
+    {
+        time_start = System.currentTimeMillis();
+        if (flag_pause) time_start -= time_that_passed;
+        flag_pause = false;
+    }
+
+    public void pause_mechanism()
+    {
+        if(flag_pause) return;
+        time_that_passed = System.currentTimeMillis() - time_start;
+        flag_pause = true;
+    }
+    
+    public void stop_mechanism()
+    {
+        if (!flag_pause) return;
+        flag_pause = true;
+        time_that_passed = 0;
+    }
+
+    public String state_mechanism(){
+        if (flag_pause)
+        {
+            return "pause";
+        } //и так далее
+        return null;
+    }
 }
