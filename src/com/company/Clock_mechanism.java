e9773502a23cbbb2f9ddf49bc78e811be468513e
User-1-win10 <panvlad2@mail.ru>
2020-11-14 12:15:56 +0300

    v 0.004

--- src/com/company/Clock_mechanism.java
+++ src/com/company/Clock_mechanism.java
@@ -5,9 +5,10 @@ import java.util.Date;
 import java.util.Scanner;
 
 
-public class Clock_mechanism extends Thread
-{static int a;
+public class Clock_mechanism extends Thread{
+static int a;
    static long time_start;
+   static boolean flag = false;
     @Override
 
     public void run(){ //предлагаю его включить до инициализации
@@ -16,64 +17,75 @@ public class Clock_mechanism extends Thread
         Date date = new Date();
         System.out.println(date.toString());
 
-        switch_case();
-
-    }
-
-    public static void switch_case(){
-        int operator;
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Выберите помодоро(1) или 2 ");
-        operator = scanner.nextInt();
-
-        switch(operator){
-            case 1: pomodoro();
-
-            break;
-        }
 
 
-
-    }
-    public static int pomodoro(){
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Выберите 30(1), 45(2), 60(3) min of pomodoro");
-        /*
-        * сделать здесь перегрузку методов, может есть библ с временем обратного отсчета?
-        * */
-        int operator_pom = scanner.nextInt();
-        switch(operator_pom){
-            case 1: remaining_time();// int x = Main.get_now_profile().work_timer;
-                long remaining_time;
-                remaining_time = Main.get_remaining_time();
-                while(true ) {
-                    System.out.println("Таймер: " + remaining_time / 60 + ":" + remaining_time % 60);
-                    try {
-                        Thread.sleep(5);
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                // a = (int) (x -(System.currentTimeMillis() - time_start)/ 1000);
-
-
-            case 2:
-                break;
-            case 3:
-                break;
-            default:
-                break;
-        }
-
-        return 0;
     }
 
 
     public static long remaining_time(){
         int x = Main.get_now_profile().work_timer;
+        int systime = (int) System.currentTimeMillis();
         //return (1800000 - System.currentTimeMillis() % 1800000 ) / 1000;
         return (x -(System.currentTimeMillis() - time_start)/ 1000 ) ;
     }
 
+    public static class Pomodoro extends Thread{
+        /*
+        * Заглушка на помодоро
+        */
+        public static long get_remaining_time(){ //как параметр основного таймера
+
+            return Clock_mechanism.remaining_time();
+
+            //return (1800000 - System.currentTimeMillis() % 1800000 ) / 1000;
+        }
+
+        public int Stop(/* передать параметры основного таймера*/){
+            get_remaining_time(); //передача параметра
+
+            return 0;
+        }
+        public int Start(){
+
+            return 0;
+        }
+        public int Pause(){
+
+            return 0;
+        }
+        public int PlusTime(/* передать параметры основного таймера*/){
+
+            return 0;
+        }
+        public String State(/* передать параметры основного таймера*/){
+
+
+            return null;
+        }
+        public void run(){
+            //что будем здесь делать
+
+        }
+        public static void main(String args[]){
+            (new Pomodoro()).start(); //запускает поток
+
+        }
+    }
+    public  class Taimer extends Thread{
+        /*
+         * Заглушка на timer
+         */
+    }
+    public  class StopWatch extends Thread{
+        /*
+         * Заглушка на секундомер
+         */
+    }
+    public  class AlarmClock extends Thread{
+        /*
+         * Заглушка на будильник
+         */
+    }
 
 }
+//добавить функц вывода времени
